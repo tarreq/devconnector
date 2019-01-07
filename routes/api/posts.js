70f6718cf3ad8d80e7a6decccd53ec1b0fc63f5d
tarreeq <tarreeq@gmail.com>
2019-01-07 21:40:58 +0200

    Jan 7 - Home

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -128,17 +128,92 @@ router.post(
       .then(post => {
         if (
           post.likes.filter(like => like.user.toString() === req.user.id)
-            .length > 0
+            .length === 0
         ) {
           return res
             .status(400)
-            .json({ notliked: "User didn't like this post" });
+            .json({ notliked: "User hanve't liked this post" });
         }
 
-        // Find user index
+        // Get remove index
+        const removeIndex = post.likes
+          .map(item => item.user.toString())
+          .indexOf(req.user.id);
 
-        // Add user id to likes array
-        post.likes.unshift({ user: req.user.id });
+        // Splice out of array
+        post.likes.splice(removeIndex, 1);
+
+        // Save post to DB
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+    //});
+  }
+);
+
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Validation vars
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // If any erros send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Profile.findOne({ user: req.user.id }).then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          // Check if comment exist
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commonetnotfound: "Comments doesn't exist" });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice out of array
+        post.comments.splice(removeIndex, 1);
 
         // Save post to DB
         post.save().then(post => res.json(post));
