3bf97c5e3dea0e0348659981bb64f87aa965801b
tarreq <tarreeq@gmail.com>
2019-01-22 21:11:51 +0200

    Jan 22 - office

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import store from "./store/store";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import PrivateRoute from "./components/common/PrivateRoute";
 
@@ -69,6 +70,14 @@ class App extends Component {
                   exact 
                   path="/edit-profile" 
                   component={EditProfile} 
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute 
+                  exact 
+                  path="/add-experience" 
+                  component={AddExperience} 
                 />
               </Switch>
             </div>
