4990e8420aa3ae0a12d13b7cb0c0578483c8aafc
tarreq <tarreeq@gmail.com>
2019-02-12 19:52:20 +0200

    Feb 12 - office

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,6 +17,12 @@ export class Profile extends Component {
             this.props.getProfileByHandle(this.props.match.params.handle)
         }
     }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.profile.profile === null && this.props.profile.loading) {
+            this.props.history.push('/not-found')
+        }
+    }
   
   render() {
       const { profile, loading } = this.props.profile
@@ -39,7 +45,10 @@ export class Profile extends Component {
                   <ProfileHeader profile={profile} />
                   <ProfileAbout profile={profile} />
                   <ProfileCreds education={profile.education} experience={profile.experience} />
-                  <ProfileGithub />
+                  
+                  {profile.githubusername ? (<ProfileGithub username={profile.githubusername} />
+                  ): null }
+                  
               </div>
           )
       }
